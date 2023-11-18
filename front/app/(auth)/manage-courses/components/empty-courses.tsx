20360bda34e2a0b5a0c8e8d43373120589f598f8
Matheus Mallmann <matheusvmallmann@gmail.com>
2023-11-18 17:49:38 -0300

    feat: Create prod docker compose

--- front/app/(auth)/manage-courses/components/empty-courses.tsx
+++ front/app/(auth)/manage-courses/components/empty-courses.tsx
@@ -2,25 +2,13 @@ import { buttonVariants } from "@/components/ui/button";
 import React from "react";
 import { cn } from "@/lib/utils";
 import Link from "next/link";
+import { MonitorPlay } from "lucide-react";
 
 export function EmptyCourses() {
   return (
     <div className="flex h-[450px] shrink-0 items-center justify-center rounded-md border border-dashed">
       <div className="mx-auto flex max-w-[420px] flex-col items-center justify-center text-center">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          stroke="currentColor"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeWidth="2"
-          className="h-10 w-10 text-muted-foreground"
-          viewBox="0 0 24 24"
-        >
-          <circle cx="12" cy="11" r="1" />
-          <path d="M11 17a1 1 0 0 1 2 0c0 .5-.34 3-.5 4.5a.5.5 0 0 1-1 0c-.16-1.5-.5-4-.5-4.5ZM8 14a5 5 0 1 1 8 0" />
-          <path d="M17 18.5a9 9 0 1 0-10 0" />
-        </svg>
+        <MonitorPlay size={40} />
 
         <h3 className="mt-4 text-lg font-semibold">Nenhum curso cadastrado</h3>
         <p className="mb-4 mt-2 text-sm text-muted-foreground">
